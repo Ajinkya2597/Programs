a806d1bdb355887da2d0d11b102f0209888a2758
Ajinkya Mhavan <54267000+Ajinkya2597@users.noreply.github.com>
2022-03-03 22:58:54 +0530

    Add files via upload

---
+++ LongestString/index.js
@@ -0,0 +1,16 @@
+var array=['HI',
+'Hello',
+'Bye',
+'longest string'];
+
+var max=0;
+var longest;
+
+for(var i=0 ; i < array.length ; i++){
+    if(array[i].length > max){
+        var max=array[i].length;
+        longest=array[i];
+    }
+}
+
+console.log(longest);
---
+++ charLargestOcurrances/index.js
@@ -0,0 +1,15 @@
+var str = 'hi ajiiii';
+var freq = {};
+var max ='';
+for(var i = 0; i < str.length; i++)
+{
+    var key = str[i];
+    if(!freq[key]){
+     freq[key] = 0;
+    }
+    freq[key]++;
+    if(max == '' || freq[key] > freq[max]){
+        max = key;
+    }
+}
+console.log("character "+max +" occures " + freq[max]+" times ");
